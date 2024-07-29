fbe233e1c611f19c035b13ccd6d94686b22be69d
Mihisanka Sandudeeptha <mihisanka1998@gmail.com>
2024-07-29 21:55:03 +0530

    internal commit

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,11 +1,18 @@
-import {createBrowserRouter} from 'react-router-dom'
+import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import Home from '../pages/Home'; // Update the path to include the 'home' directory
 
-const router = createBrowserRouter ([
-    {
-        path: "/",
-        element: <App/>,
-    }
-])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+    ],
+  },
+]);
 
 export default router;
